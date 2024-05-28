e148c0502edd958e99708b2b4f5e5a75abc74e90
ArcGate-Govind <govind@arcgate.com>
2024-05-28 16:00:24 +0530

    project-template

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <>
+    <div className='outerDiv' >
+      <div className='innerDiv'>
+          <div className='inderDiv-animation'></div>
+          <div className='inderDiv-animation'></div>
+      </div>
+    </div>
+   </>
+  );
+}
+
+export default App;
